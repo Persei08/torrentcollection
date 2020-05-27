6f88ee823b4bece04ff9f2880d352a301d12c8ba
Persei08 <Persei08@users.noreply.github.com>
2020-05-27 09:31:08 +0200

    add omgtorrent

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const leet            = require('xtorrent');
 const rbg             = require('torrentapi-wrapper2');
 const tpb             = require('thepiratebay');
+const omg             = require('omgtorrent');
 module.exports = {
   leet: leet,
   rbg: rbg,
-  tpb: tpb
+  tpb: tpb,
+  omg: omg
 };
